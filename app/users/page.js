1caf25ffde5dee70b7a10e6881ecb90748134c7e
Erik <erkgarciae@gmail.com>
2024-04-04 00:47:49 -0400

    fixed id URL

--- app/users/page.js
+++ app/users/page.js
@@ -3,6 +3,6 @@ import Link from 'next/link';
 export default async function Users(){
     const users=await User.findAll();
     return(
-        users.map(user=><div><img src={user.image_url}/><Link href={`/users/${user.userid}`}>{user.username}</Link></div>)
+        users.map(user=><div><img src={user.image_url}/><Link href={`/users/${user.id}`}>{user.username}</Link></div>)
     )
 }
